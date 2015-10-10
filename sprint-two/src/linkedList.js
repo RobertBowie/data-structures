296586f33274f06d46485dbfb57079a52dbb6b36
RobertBowie <robertb1903@gmail.com>
2015-10-10 09:20:24 -0700

    Added linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,35 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = new Node(value);
+    var tail = list.tail;
+
+    if (tail) {
+      tail.next = node;
+    } else {
+      list.head = node;
+    }
+
+    list.tail = node;
+
+    return node;
   };
 
   list.removeHead = function(){
+    var headToBeRemoved = list.head;
+    list.head = list.head.next;
+    headToBeRemoved.next = null;
+    return headToBeRemoved.value;
   };
 
-  list.contains = function(target){
+  list.contains = function(target, node){
+    node = node || list.head;
+
+    if (!node) { return false; }
+    if (node.value === target) { return true; }
+    if (node === list.tail) { return false; }
+
+    return (list.contains(target, node.next));
   };
 
   return list;
