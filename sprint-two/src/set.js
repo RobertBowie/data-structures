3cf1509f17356a8530ccc027fe0d1a09d0be15e6
RobertBowie <robertb1903@gmail.com>
2015-10-10 10:48:54 -0700

    Added set.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,35 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  if (!this.contains(item)) {
+    this._storage.push(item);
+    return 0;
+  }
+  return -1;
 };
 
 setPrototype.contains = function(item){
+  return this.find(item) > -1;
 };
 
 setPrototype.remove = function(item){
+  var index = this.find(item);
+  if (index > -1) {
+    this._storage.splice(index, 1);
+    return 0;
+  }
+  return -1;
 };
 
+setPrototype.find = function(item){
+  return this._storage.indexOf(item);
+}
 /*
  * Complexity: What is the time complexity of the above functions?
  */
