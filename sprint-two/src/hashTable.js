192961bf5c8add54729245a02b36a1422fba863a
RobertBowie <robertb1903@gmail.com>
2015-10-10 11:41:27 -0700

    Added hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,38 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i) || [];
+
+  if (bucket.length) {
+    bucket.forEach(function(item) {
+      if (item[0] === k) {
+        item[1] = v;
+      } else {
+        bucket.push([k, v]);    
+      }
+    });
+  } else {
+    bucket.push([k, v]);
+  }
+
+  this._storage.set(i, bucket);
+  return 0;
 };
 
 HashTable.prototype.retrieve = function(k){
+  var result = null;
   var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.get(i).forEach (function(item){
+    if (item[0] === k) {
+      result = result || item[1];
+    }
+  });
 
+  return result;
 };
 
 HashTable.prototype.remove = function(k){
-
+  this.insert(k, null);  
 };
 
 
